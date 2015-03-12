5a0d2396e9747a3fde31ccb4ec852867fa83b476
julienbrault <jrbrault@gmail.com>
2015-03-11 20:18:33 -0400

    nouveau fichier cname

--- devoir1-javascript.js
+++ devoir1-javascript.js
@@ -1,14 +1,14 @@
 /**
  * Created by julienbrault on 15-03-07.
  */
-alert("bienvenue!")
+alert("bienvenue!");
 
 var age= prompt("Quel est votre âge?");
 
-console.log("Je comprends que votre âge, en nombre d'année terrestre, est"+"age")
+console.log("Je comprends que votre âge, en nombre d'année terrestre, est"+"age");
 
 
-var nombre= prompt("Entrez un nombre entre 1 et 100")
+var nombre= prompt("Entrez un nombre entre 1 et 100");
 
 if (nombre>= 1 && nombre<=100)
 {alert("merci")}
@@ -20,7 +20,7 @@ if (nombre<1 || nombre>100)
 
 var mys=24;
 
-var essai1= prompt("Essayez de deviner le nombre que j'ai choisi entre 1 et 100")
+var essai1= prompt("Essayez de deviner le nombre que j'ai choisi entre 1 et 100");
 
 if (essai1<1 || essai1>100)
 {alert("mauvaise entrée")}
@@ -34,20 +34,22 @@ if (essai1<mys && essai1>1)
 if (essai1>mys && essai1<100)
 {alert("Votre nombre est trop grand.")}
 
-for (y=0;y<=4;y++){
+
+for(var y=0;y<=4;y++) {
     var mys=24;
 
-    var essai1= prompt("Vous avez 5 esssais pour deviner le nombre que j'ai choisi entre 1 et 100")
+    var essai1= prompt("Vous avez 5 esssais pour deviner le nombre que j'ai choisi entre 1 et 100");
 
     if (essai1<1 || essai1>100)
     {alert("mauvaise entrée")}
 
-    if (essai1==mys)
-    {alert("Vous avez trouvé le nombre")}
+    if else (essai1==mys);
+    {alert("Vous avez trouvé le nombre"); break}
 
-    if (essai1<mys && essai1>1)
+
+    if else (essai1<mys && essai1>1);
     {alert("Votre nombre est trop petit.")}
 
-    if (essai1>mys && essai1<100)
+    if else (essai1>mys && essai1<100);
     {alert("Votre nombre est trop grand.")}
 }
